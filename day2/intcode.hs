1a27fdbcea7cc3740f564013b66081f668094736
Aritra Sarkar <aritra1911@yahoo.com>
2020-11-17 18:24:23 +0530

    Refactor Intcode with improved terminology

--- day2/intcode.hs
+++ day2/intcode.hs
@@ -1,15 +1,15 @@
-type Program = [Int]
+type Memory = [Int]
 
--- pointer points to the instruction to be read
-data ProgramState = ProgramState { pointer :: Int
-                                 , program :: Program
-                                 } deriving (Show, Eq)
+-- Here `pointer` refers to the instruction pointer
+data State = State { pointer :: Int
+                   , memory  :: Memory
+                   } deriving (Show, Eq)
 
-data OpCode = NOP | ADD | MUL | HLT deriving (Show,Eq,Ord)
+data OpCode = NOP | ADD | MUL | HLT deriving (Show, Eq, Ord)
 
--- ProgramStates don't care about the program itself
-instance Ord ProgramState where
-    (ProgramState p _) `compare` (ProgramState q _) = p `compare` q
+-- Memory contents doesn't matter when comparing states
+instance Ord State where
+    state1 `compare` state2 = (pointer state1) `compare` (pointer state2)
 
 -- https://hackage.haskell.org/package/ilist-0.4.0.1/docs/Data-List-Index.html#v:setAt
 setAt :: Int -> a -> [a] -> [a]
@@ -28,6 +28,11 @@ intToOpcode instruction = case instruction of
     99 -> HLT
     _  -> NOP
 
+stepVal :: OpCode -> Int
+stepVal NOP = 1
+stepVal HLT = 1
+stepVal _   = 4
+
 -- TODO: HLT /= NOP
 eval :: OpCode -> Int -> Int -> Maybe Int
 eval ADD x y = Just $ x + y
@@ -35,26 +40,31 @@ eval MUL x y = Just $ x * y
 eval HLT _ _ = Nothing
 eval NOP _ _ = Nothing
 
-execute :: ProgramState -> ProgramState
-execute (ProgramState ptr prog) =
-    let code = intToOpcode $ prog !! ptr
-        result = eval code (val $ arg 1) (val $ arg 2)
+execute :: State -> State
+execute state =
+    let opcode  = intToOpcode $ (memory state) !! (pointer state)
+        nextPtr = (pointer state) + (stepVal opcode)
+        result  = eval opcode (val $ arg 1) (val $ arg 2)
     in case result of
-        Just x -> execute $ ProgramState {pointer = ptr + 4, program = setAt (arg 3) x prog}
-        Nothing -> ProgramState {pointer = 0, program = prog}
-    where arg x = prog !! (ptr + x)
-          val x = prog !! x
+        Just x  -> execute $ State { pointer = nextPtr
+                                   , memory  = setAt (arg 3) x (memory state)
+                                   }
+        Nothing -> State { pointer = nextPtr
+                         , memory = memory state
+                         }
+    where arg x = (memory state) !! ((pointer state) + x)
+          val x = (memory state) !! x
 
-getOutput :: ProgramState -> String
-getOutput (ProgramState _ state) = show $ state !! 0
+getOutput :: State -> String
+getOutput state = show $ (memory state) !! 0
 
-initExec :: Program -> ProgramState
-initExec prog = ProgramState {pointer = 0, program = prog}
+initExec :: Memory -> State
+initExec contents = State { pointer = 0, memory = contents }
 
-parse :: String -> Program
+parse :: String -> Memory
 parse input = read $ '[':input ++ "]"
 
-restore1202 :: Program -> Program
+restore1202 :: Memory -> Memory
 restore1202 (x:_:_:rest) = x:12:02:rest
 
 main = do
