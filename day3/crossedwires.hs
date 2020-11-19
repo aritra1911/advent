4a0b935435aa6553fc8aae23d24e4fdfc0e35fd2
Aritra Sarkar <aritra1911@yahoo.com>
2020-11-19 23:50:54 +0530

    Add data type and parse moves

--- day3/crossedwires.hs
+++ day3/crossedwires.hs
@@ -1,5 +1,16 @@
-parseInput :: String -> [String]
-parseInput = words . (map commasToSpaces)
+data Move d = Up d | Left' d | Down d | Right' d
+    deriving (Show, Eq)
+
+parseMove :: String -> Move Int
+parseMove (move:distance) =
+    case move of
+        'U' -> Up     $ read distance
+        'L' -> Left'  $ read distance
+        'D' -> Down   $ read distance
+        'R' -> Right' $ read distance
+
+parseInput :: String -> [Move Int]
+parseInput = map parseMove . words . map commasToSpaces
     where commasToSpaces c = if c == ',' then ' ' else c
 
-main = interact $ unlines . (map show) . (map parseInput) . lines
+main = interact $ unlines . map show . map parseInput . lines
