a8731457067b9fe08f2bd463c72508567b326391
Aritra Sarkar <aritra1911@yahoo.com>
2021-12-03 18:47:27 +0530

    Day 3 : Part 3

--- 2021/binary_diagnostic/src/main.rs
+++ 2021/binary_diagnostic/src/main.rs
@@ -22,6 +22,11 @@ fn main() {
 
     let (gamma_rate, epsilon_rate) = calculate_rates(&report);
     println!("Answer to Part One : {}", gamma_rate * epsilon_rate);
+
+    let oxygen_generator_rating = calculate_oxygen_rating(&report);
+    let co2_scrubber_rating = calculate_co2_scrubber_rating(&report);
+    println!("Answer to Part Two : {}",
+             oxygen_generator_rating * co2_scrubber_rating);
 }
 
 fn calculate_rates(report: &Vec<String>) -> (u32, u32) {
@@ -66,4 +71,74 @@ fn calculate_rates(report: &Vec<String>) -> (u32, u32) {
     let epsilon_rate = !gamma_rate & ((1 << n_bits) - 1);
 
     (gamma_rate, epsilon_rate)
+}
+
+fn calculate_oxygen_rating(report: &Vec<String>) -> u32 {
+
+    let mut numbers: Vec<String> = report.clone();
+    let mut idx = 0;
+    let mut nums = numbers.len();
+
+    loop {
+        let mut ones = 0usize;
+
+        for bin_number in numbers.clone() {
+            if bin_number.as_bytes()[idx] == '1' as u8 { ones += 1 }
+        }
+
+        let maj = if {
+            let zeros = nums - ones;
+            ones >= zeros
+        } { '1' as u8 } else { '0' as u8 };
+
+        numbers = numbers.into_iter()
+                         .filter(|num| num.as_bytes()[idx] == maj)
+                         .collect();
+
+        nums = numbers.len();
+
+        if nums == 1 {
+            break;
+        }
+
+        idx += 1;
+    }
+
+    let num = numbers.pop().unwrap();
+    u32::from_str_radix(num.as_str(), 2).unwrap()
+}
+
+fn calculate_co2_scrubber_rating(report: &Vec<String>) -> u32 {
+
+    let mut numbers: Vec<String> = report.clone();
+    let mut idx = 0;
+    let mut nums = numbers.len();
+
+    loop {
+        let mut ones = 0usize;
+
+        for bin_number in numbers.clone() {
+            if bin_number.as_bytes()[idx] == '1' as u8 { ones += 1 }
+        }
+
+        let min = if {
+            let zeros = nums - ones;
+            ones < zeros
+        } { '1' as u8 } else { '0' as u8 };
+
+        numbers = numbers.into_iter()
+                         .filter(|num| num.as_bytes()[idx] == min)
+                         .collect();
+
+        nums = numbers.len();
+
+        if nums == 1 {
+            break;
+        }
+
+        idx += 1;
+    }
+
+    let num = numbers.pop().unwrap();
+    u32::from_str_radix(num.as_str(), 2).unwrap()
 }
