3309ddc1cdf21a24f726352089cccb8d0ffcff2c
Aritra Sarkar <aritra1911@yahoo.com>
2021-12-14 20:42:54 +0530

    Day 14 : Part 2 Proper

--- 2021/extended_polymerization/src/main.rs
+++ 2021/extended_polymerization/src/main.rs
@@ -3,50 +3,80 @@ use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 use itertools::Itertools;
 use std::collections::HashMap;
-use std::str;
 
-fn pair_insert(polymer: &mut Vec<u8>, rules: &HashMap<&str, u8>) {
+fn pair_insert(polymer_pairs: &mut HashMap<String, usize>,
+                       rules: &HashMap<&str, u8>) {
 
-    let mut new_elements = Vec::new();
+    let mut new_pairs: HashMap<String, usize> = HashMap::new();
 
-    for i in 0..(polymer.len() - 1) {
-        let pair = [polymer[i], polymer[i + 1]];
-        let pair = str::from_utf8(&pair).unwrap();
-        new_elements.push(rules.get(pair).unwrap());
+    for (pair, count) in polymer_pairs.iter() {
+        let element = rules.get(pair.as_str()).unwrap();
+        let a = pair.as_bytes()[0];
+        let b = pair.as_bytes()[1];
+        let first_pair_arr = vec![a, *element];
+        let second_pair_arr = vec![*element, b];
+        let first_pair = String::from_utf8(first_pair_arr).unwrap();
+        let second_pair = String::from_utf8(second_pair_arr).unwrap();
+
+        match new_pairs.get_mut(&first_pair) {
+            Some(v) => { *v += *count; },
+            None => { new_pairs.insert(first_pair, *count); },
+        }
+
+        match new_pairs.get_mut(&second_pair) {
+            Some(v) => { *v += *count; },
+            None => { new_pairs.insert(second_pair, *count); },
+        }
     }
 
-    let mut new_polymer = Vec::new();
-    for (&a, &b) in polymer.iter().zip(new_elements) {
-        new_polymer.push(a);
-        new_polymer.push(b);
-    }
-    new_polymer.push(*polymer.last().unwrap());
-
-    *polymer = new_polymer.clone();
+    *polymer_pairs = new_pairs.clone();
 }
 
 #[allow(unused_variables)]
-fn part1(template: &String, rules: &HashMap<&str, u8>) -> usize {
+fn solve(template: &String, rules: &HashMap<&str, u8>, steps: usize) -> usize {
 
-    let mut polymer = template
-        .chars()
-        .map(|c| c as u8)
-        .collect();
+    let mut polymer_pairs = HashMap::new();
 
-    let steps = 10;
-    for step in 1..=steps {
-        pair_insert(&mut polymer, &rules);
+    for i in 0..(template.len() - 1) {
+        let bytes = template.as_bytes();
+        let pair_arr = vec![bytes[i], bytes[i + 1]];
+        let pair = String::from_utf8(pair_arr).unwrap();
+
+        match polymer_pairs.get_mut(&pair) {
+            Some(count) => { *count += 1; },
+            None => { polymer_pairs.insert(pair, 1); },
+        }
     }
 
-    let mut distinct_elements = polymer.clone();
-    distinct_elements.sort_unstable();
-    distinct_elements.dedup();
+    let last_element = template.as_bytes()[template.len() - 1];
 
-    let (mut min, mut max) = (usize::MAX, 0);
-    for element in distinct_elements {
-        let count = polymer.iter().filter(|&&e| e == element).count();
-        if count > max { max = count; }
-        if count < min { min = count; }
+    for step in 1..=steps {
+        pair_insert(&mut polymer_pairs, &rules);
+    }
+
+    let mut elements: Vec<u8> = polymer_pairs.iter()
+        .map(|(pair, _)| pair.as_bytes()[0]).collect();
+
+    elements.push(last_element);
+    elements.sort_unstable();
+    elements.dedup();
+
+    let (mut max, mut min) = (0, usize::MAX);
+    for element in elements {
+        let mut total_count = 0;
+
+        for (pair, count) in polymer_pairs.iter() {
+            if element == pair.as_bytes()[0] {
+                total_count += count;
+            }
+        }
+
+        /* Don't miss the last element which does not appear
+         * within the first characters of the pairs */
+        total_count += if element == last_element { 1 } else { 0 };
+
+        if total_count > max { max = total_count; }
+        if total_count < min { min = total_count; }
     }
 
     max - min
@@ -81,6 +111,9 @@ fn main() {
         rules.insert(key, value);
     }
 
-    let difference = part1(&template, &rules);
+    let difference = solve(&template, &rules, 10);
     println!("Answer to Part One : {}", difference);
+
+    let difference = solve(&template, &rules, 40);
+    println!("Answer to Part Two : {}", difference);
 }
