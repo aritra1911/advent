5d966adf75538da3d8018bc4579b43346c5ea6bb
Aritra Sarkar <aritra1911@yahoo.com>
2021-12-15 17:06:17 +0530

    Day 15 : Part 1 (Dijkstra)

---
+++ 2021/chiton/src/main.rs
@@ -0,0 +1,108 @@
+use std::env;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+use std::cmp::min;
+
+fn get_neighbours(   i: usize,    j: usize,
+                  rows: usize, cols: usize) -> Vec<(usize, usize)> {
+
+    let mut neighbours = Vec::new();
+
+    if i > 0        { neighbours.push((i - 1, j)); }
+    if i < rows - 1 { neighbours.push((i + 1, j)); }
+    if j > 0        { neighbours.push((i, j - 1)); }
+    if j < cols - 1 { neighbours.push((i, j + 1)); }
+
+    neighbours
+}
+
+fn dijkstra(risk_levels: &Vec<Vec<u8>>, source: (usize, usize)) -> Vec<Vec<u64>> {
+
+    let rows = risk_levels.len();
+    let cols = risk_levels[0].len();
+    let mut visited = vec![vec![false; cols]; rows];
+    let mut dist = vec![vec![u64::MAX; cols]; rows];
+    let mut queue = Vec::new();
+
+    let (src_i, src_j) = source;
+    dist[src_i][src_j] = 0;
+
+    /* Add all vertices to queue */
+    for i in 0..rows {
+        for j in 0..cols {
+            if i == src_i && j == src_j { continue; }
+            queue.push((i, j));
+        }
+    }
+    queue.push((src_i, src_j));
+
+    while let Some(vertex) = queue.pop() {
+        let (i, j) = vertex;
+
+        for neighbour in get_neighbours(i, j, rows, cols) {
+            let (n_i, n_j) = neighbour;
+
+            if !visited[n_i][n_j] {
+                let new_dist =  dist[i][j] + risk_levels[n_i][n_j] as u64;
+                dist[n_i][n_j] = min(new_dist, dist[n_i][n_j]);
+            }
+        }
+
+        visited[i][j] = true;
+        queue.sort_unstable_by(|(i, j), (k, l)| dist[*k][*l].cmp(&dist[*i][*j]));
+    }
+
+    dist
+}
+
+fn part1(risk_levels: &Vec<Vec<u8>>) -> u64 {
+
+    let distances = dijkstra(risk_levels, (0, 0));
+    let rows = risk_levels.len();
+    let cols = risk_levels[0].len();
+
+    /*
+    for i in 0..rows {
+        for j in 0..cols {
+            print!(" {:2} ", distances[i][j]);
+        }
+        println!();
+    }
+    */
+
+    distances[rows - 1][cols - 1]
+}
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+
+    let input: Vec<Vec<u8>> = if args.len() > 1 && args[1] != "-" {
+        let file = File::open(&args[1]).unwrap();
+        let reader = BufReader::new(file);
+
+        reader
+            .lines()
+            .map(|line| line.unwrap()
+                 .trim()
+                 .to_string()
+                 .chars()
+                 .map(|c| c.to_digit(10).unwrap() as u8)
+                 .collect())
+            .collect()
+    } else {
+        stdin()
+            .lock()
+            .lines()
+            .map(|line| line.unwrap()
+                 .trim()
+                 .to_string()
+                 .chars()
+                 .map(|c| c.to_digit(10).unwrap() as u8)
+                 .collect())
+            .collect()
+    };
+
+    let shortest_distance = part1(&input);
+    println!("Answer to Part One : {}", shortest_distance);
+}
